3cc7cc1371ee0880581748aa82f61b2ac81f1a2d
dilap54 <dilap54@mail.ru>
2018-02-23 22:20:03 +0300

    add pairs

--- rsscan.dpr
+++ rsscan.dpr
@@ -42,7 +42,7 @@ var
   I: Integer;
   IPint: LongWord;
   Module: TModuleDesc;
-  Pairs: String;
+  BasicPairs: String;
   IP: TIPv4;
   Port: Word;
   hRouter: Pointer;
@@ -73,20 +73,142 @@ begin
   SetParam(stUseCustomPage, Pointer(False));
   SetParam(stDualAuthCheck, Pointer(False));
 
-  Pairs := 'admin'#9'admin'#13#10+
-           'admin'#9'password'#13#10+
-           'admin'#9'123321Aa'#13#10+
-           'admin'#9'1234'#13#10;
-  if not SetParam(stPairsBasic, PChar(Pairs)) then begin
-	Writeln(Pairs);
+  BasicPairs := 'admin'#9''#13#10+
+    'admin'#9'admin'#13#10+
+    'admin'#9'1234'#13#10+
+    'admin'#9'password'#13#10+
+    'Admin'#9'Admin'#13#10+
+    ''#9'admin'#13#10+
+    'root'#9''#13#10+
+    'root'#9'admin'#13#10+
+    'root'#9'root'#13#10+
+    'root'#9'public'#13#10+
+    'admin'#9'nimda'#13#10+
+    'admin'#9'adminadmin'#13#10+
+    'admin'#9'gfhjkm'#13#10+
+    'admin'#9'airlive'#13#10+
+    'airlive'#9'airlive'#13#10+
+    ''#9'root'#13#10+
+    ''#9''#13#10+
+    'support'#9''#13#10+
+    'support'#9'support'#13#10+
+    'super'#9'super'#13#10+
+    'super'#9'APR@xuniL'#13#10+
+    'super'#9'zxcvbnm,./'#13#10+
+    'admin'#9'onlime'#13#10+
+    'super'#9'asong'#13#10+
+    'admin'#9'mts'#13#10+
+    'mts'#9'mts'#13#10+
+    'telecomadmin'#9'admintelecom'#13#10+
+    'mgts'#9'mtsoao'#13#10+
+    'admin'#9'Uq-4GIt3M'#13#10+
+    ''#9'aDm1n$TR8r'#13#10+
+    'kyivstar'#9'kyivstar'#13#10+
+    'admin'#9'0508780503'#13#10+
+    'telekom'#9'telekom'#13#10+
+    'superadmin'#9'Is$uper@dmin'#13#10+
+    'admin'#9'dPZb4GJTu9'#13#10+
+    'admin'#9'2w4f6n8k'#13#10+
+    'admin'#9'szt'#13#10+
+    'admin'#9'radmin'#13#10+
+    'admin'#9'RTadmin1979'#13#10+
+    'engineer'#9'amplifier'#13#10+
+    'admin'#9'newit43'#13#10+
+    'superadmin'#9'Jkbvgbflf2014'#13#10+
+    'admin'#9'Jkbvgbflf2014'#13#10+
+    'admin'#9'1'#13#10+
+    'admin'#9'123'#13#10+
+    'admin'#9'0000'#13#10+
+    'admin'#9'00000000'#13#10+
+    'admin'#9'12345'#13#10+
+    'admin'#9'123456'#13#10+
+    'admin'#9'1234567'#13#10+
+    'admin'#9'12345678'#13#10+
+    'admin'#9'123456789'#13#10+
+    'admin'#9'1234567890'#13#10+
+    'admin'#9'qwerty'#13#10+
+    'admin'#9'beeline'#13#10+
+    'admin'#9'beeline2013'#13#10+
+    'admin'#9'iyeh'#13#10+
+    'admin'#9'ghbdtn'#13#10+
+    'admin'#9'admin225'#13#10+
+    'admin'#9'rombik1'#13#10+
+    'admin'#9'ho4uku6at'#13#10+
+    'admin'#9'juklop'#13#10+
+    'admin'#9'free'#13#10+
+    'admin'#9'inet'#13#10+
+    'admin'#9'internet'#13#10+
+    'admin'#9'asus'#13#10+
+    'admin'#9'root'#13#10+
+    'admin'#9'ADMIN'#13#10+
+    'admin'#9'adsl'#13#10+
+    'admin'#9'adslroot'#13#10+
+    'admin'#9'adsladmin'#13#10+
+    'admin'#9'Ferum'#13#10+
+    'admin'#9'Ferrum'#13#10+
+    'admin'#9'FERUM'#13#10+
+    'admin'#9'FERRUM'#13#10+
+    'admin'#9'Kendalf9'#13#10+
+    'admin'#9'263297'#13#10+
+    'admin'#9'590152'#13#10+
+    'admin'#9'21232'#13#10+
+    'admin'#9'adn8pzszk'#13#10+
+    'admin'#9'amvqnekk'#13#10+
+    'admin'#9'biyshs9eq'#13#10+
+    'admin'#9'e2b81d_1'#13#10+
+    'admin'#9'Dkdk8e89'#13#10+
+    'admin'#9'flvbyctnb'#13#10+
+    'admin'#9'qweasdOP'#13#10+
+    'admin'#9'EbS2P8'#13#10+
+    'admin'#9'FhF8WS'#13#10+
+    'admin'#9'ZmqVfo'#13#10+
+    'admin'#9'ZmqVfo1'#13#10+
+    'admin'#9'ZmqVfo2'#13#10+
+    'admin'#9'ZmqVfo3'#13#10+
+    'admin'#9'ZmqVfo4'#13#10+
+    'admin'#9'ZmqVfoVPN'#13#10+
+    'admin'#9'ZmqVfoSIP'#13#10+
+    'admin'#9'ZmqVfoN1'#13#10+
+    'admin'#9'ZmqVfoN2'#13#10+
+    'admin'#9'ZmqVfoN3'#13#10+
+    'admin'#9'ZmqVfoN4'#13#10+
+    'admin'#9'9f4r5r79//'#13#10+
+    'admin'#9'airocon'#13#10+
+    'admin'#9'zyxel'#13#10+
+    'adsl'#9'realtek'#13#10+
+    'osteam'#9'5up'#13#10+
+    'root'#9'toor'#13#10+
+    'ZXDSL'#9'ZXDSL'#13#10+
+    ''#9'support'#13#10+
+    ''#9'Cisco'#13#10+
+    'Cisco'#9'Cisco'#13#10+
+    ''#9'cisco'#13#10+
+    'cisco'#9'cisco'#13#10+
+    'admin'#9'default'#13#10+
+    'admin'#9'cisco'#13#10+
+    'admin'#9'changeme'#13#10+
+    ''#9'c'#13#10+
+    ''#9'cc'#13#10+
+    ''#9'Cisco router'#13#10+
+    ''#9'letmein'#13#10+
+    ''#9'_Cisco'#13#10+
+    'enable'#9'cisco'#13#10+
+    'pnadmin'#9'pnadmin'#13#10+
+    'root'#9'attack'#13#10+
+    'root'#9'Cisco'#13#10+
+    'user'#9''#13#10+
+    'user'#9'user'#13#10+
+    'admin'#9'123321Aa'#13#10+
+    ''#9'user';
+  if not SetParam(stPairsBasic, PChar(BasicPairs)) then begin
     Writeln('Failed to load Basic Authentication pairs');
     Exit;
   end;
-  if not SetParam(stPairsDigest, PChar(Pairs)) then begin
+  if not SetParam(stPairsDigest, PChar(BasicPairs)) then begin
     Writeln('Failed to load Digest Authentication pairs');
     Exit;
   end;
-  if not SetParam(stPairsForm, PChar(Pairs)) then begin
+  if not SetParam(stPairsForm, PChar(BasicPairs)) then begin
     Writeln('Failed to load Form Authentication pairs');
     Exit;
   end;
